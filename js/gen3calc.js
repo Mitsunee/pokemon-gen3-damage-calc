2eeeb46bbcaea79fc6014283708a8cf511346c86
Mitsunee <michelle@renzio42.com>
2017-09-14 17:01:37 +0200

    [WIP] Switched from Math.trunc to bitwise operator magic
    Shoutouts to gifvex!

--- js/gen3calc.js
+++ js/gen3calc.js
@@ -24,30 +24,25 @@ function DamageCalc(){
 	output += defStage + " " + D + " with " + HP + "HP:\n";
 	
 	////-- modify stats based boost --
-	if(input.badgeBoostAtk.checked) A = Math.trunc(A*1.1);//10% Badgeboost to ATK
-	if(input.badgeBoostDef.checked) D = Math.trunc(D*1.1);//10% Badgeboost to DEF
-	if(input.itemBoost.checked) A = Math.trunc(A*1.1);//10% Itemboost
-	if(input.abilityBoost.checked) BP = Math.trunc(BP*1.5);//50% Basepower boost from Ability
+	if(input.badgeBoostAtk.checked) A =0| A*1.1;//10% Badgeboost to ATK
+	if(input.badgeBoostDef.checked) D =0| D*1.1;//10% Badgeboost to DEF
+	if(input.itemBoost.checked) A =0| A*1.1;//10% Itemboost
+	if(input.abilityBoost.checked) BP =0| BP*1.5;//50% Basepower boost from Ability
 	//AtkStages
 	if(atkStage > 0) {
         SM = (10 + (5 * atkStage)) / 10;
 	} else if(atkStage < 0) {
         SM = 10 / (10 + (-5 * atkStage));
 	} else  SM = 1;
-	A = Math.trunc(A*SM);//Apply atkStage multiplier to atkStat
+	A =0| A*SM;//Apply atkStage multiplier to atkStat
 	//DefStages
 	if(defStage > 0) {
 		SM = (10 + (5 * defStage)) / 10;
 	} else if(defStage < 0) {
 		SM = 10 / (10 + (-5 * defStage));
 	} else  SM = 1;
-	D = Math.trunc(D*SM);//Apply defStage multiplier to defStat
-	if(outputVerbose) {
-		console.log("Attack stat after boosts: "+A);
-		console.log("Basepower after boosts: "+BP);
-		console.log("Defense stat after boosts: "+D);
-	}
-
+	D =0| D*SM;//Apply defStage multiplier to defStat
+	
 	////-- calculate base damage --
 	basedamage = Math.trunc(Math.trunc(Math.trunc(2*L/5+2)*A*BP/D)/50);
 	
@@ -56,23 +51,23 @@ function DamageCalc(){
 	if(input.doublecheck.checked) {doublecheck=true;} else {doublecheck=false;}
 	if(input.lsrefcheck.checked) {lsrefcheck=true;} else {lsrefcheck=false;}
 	if(lsrefcheck && doublecheck===false) {//Light Screen or Reflect apply hitting one target
-		basedamage = Math.trunc(basedamage/2)
+		basedamage =0| basedamage/2;
 	} else if(lsrefcheck && doublecheck) {//Light Screen or Reflect apply hitting both targets
-		basedamage = Math.trunc((basedamage*2)/3)
+		basedamage =0| (basedamage*2)/3;
 	}
-	if(doublecheck) basedamage = Math.trunc(basedamage/2);
+	if(doublecheck) basedamage =0| basedamage/2;
 	//Weather modifiers
-	if(weathercheck=="goodweather") basedamage = Math.trunc(basedamage*1.5);
-	if(weathercheck=="badweather") basedamage = Math.trunc(basedamage/2);
+	if(weathercheck=="goodweather") basedamage =0| basedamage*1.5;
+	if(weathercheck=="badweather") basedamage =0| basedamage/2;
 	basedamage += 2;
 	//factor in STAB and type-effectiveness
-	if(input.stabcheck.checked) basedamage = Math.trunc(basedamage*1.5);
-	damage = basedamage * radioValue("typeeffect");
+	if(input.stabcheck.checked) basedamage =0| basedamage*1.5;
+	damage =0| basedamage * radioValue("typeeffect");
 
 	////-- damage rolls --
 	missedrange = 0;
 	for (i=85;i<101;i++) {
-		roll = Math.trunc((damage*i)/100);
+		roll =0| (damage*i)/100;
 		if(input.outputVerbose.checked) {
 			output += roll;
 			if(i != 100) output += ", ";
@@ -80,7 +75,7 @@ function DamageCalc(){
 		if(HP - roll > 0) missedrange += 1;
 	}
 	OHKO = 100 * ((16 - missedrange)/16);
-	minroll = Math.trunc((damage*85)/100);
+	minroll =0| (damage*85)/100;
 	//Calculating damage in %
 	percminroll = minroll / HP * 100;
 	percdamage = damage / HP * 100;
