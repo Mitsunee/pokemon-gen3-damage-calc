a5e27a0992c04b96b88e78044f35f80ef21ebcf5
Mitsunee <michelle@renzio42.com>
2019-01-13 11:50:41 +0100

    Fixed Bug: Forgot to hide empty result error...
    ...rookie mistakes are the best.

--- js/pokemonSearch.js
+++ js/pokemonSearch.js
@@ -1,24 +1,30 @@
 function pokeSearch(that) {//that is the calling form element's this
-	let haystack;
+	let haystack,icon,errorDiv;
 	switch(that.name) {
 		case "defenderName":
 			haystack = document.querySelector("#search-defender>table>tbody").children;
+			icon=$("#defender-icon")[0];
+			errorDiv=$("#search-defender-empty-result");
+			errorDiv.hide();
 			break;
 		default:
 			return false;
 	}
 	if(that.value == "") {
 		for(let i of haystack) i.style.display="table-row";//show all rows
-		$("#defender-icon")[0].src="i/mons/icons/000.png";
+		icon.src="i/mons/icons/000.png";
 		return false;
 	}
+	let foundMon=false;
 	for(let i of haystack) {
 		if(i.dataset.pokemonname.indexOfIgnoreCase(that.value)!==-1) {
 			i.style.display="table-row";
+			foundMon=true;
 		} else {
 			i.style.display="none";
 		}
 	}
+	if(foundMon==false) errorDiv.show();
 }
 function pokeSearchPick(that,what) {//that is the selected pokemon's this, what is currently always "defender" until I add more stuff LOL
 	//validate proper use of function
