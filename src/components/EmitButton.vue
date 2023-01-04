70eee6a28d0c7a17753aa94f18c71bac9eee6d78
Mitsunee <19474909+Mitsunee@users.noreply.github.com>
2023-01-04 14:48:22 +0100

    add: EmitButton component

---
+++ src/components/EmitButton.vue
@@ -0,0 +1,43 @@
+<template>
+  <button
+    @click="!disabled && $emit('btn-click')"
+    :class="[color || '', disabled ? 'disabled' : '']"
+    :aria-disabled="disabled">
+    <slot>Button</slot>
+  </button>
+</template>
+
+<script setup lang="ts">
+import type { UiColor } from "@/types/enum";
+
+defineProps<{
+  color?: UiColor;
+  disabled?: true;
+}>();
+</script>
+
+<style scoped>
+button {
+  display: inline-block;
+  border: 1px outset var(--ui-border);
+  border-radius: 0.5rem;
+  background-color: var(--ui-bg);
+  font-family: inherit;
+  color: var(--ui);
+  transition: transform 0.1s ease-in-out;
+}
+
+button:not(.disabled):hover,
+button:not(.disabled):focus {
+  transform: scale(1.05);
+}
+
+button:not(.disabled):active {
+  box-shadow: inset 0px 0.5px 1px var(--shadow);
+}
+
+button.disabled {
+  opacity: 0.4;
+  filter: grayscale(0.5);
+}
+</style>
---
+++ src/types/enum.ts
@@ -0,0 +1,6 @@
+export const enum UiColor {
+  BLUE = "",
+  RED = "ui-red",
+  GREEN = "ui-green",
+  ORANGE = "ui-orange"
+}
